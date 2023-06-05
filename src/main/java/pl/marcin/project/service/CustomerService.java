2364adcfc2b9ea59f36cdb93c28e9ff27b8c952f
Marcin <gasiorek.mail@gmail.com>
2023-06-05 13:36:44 +0200

    change customer service

--- src/main/java/pl/marcin/project/service/CustomerService.java
+++ src/main/java/pl/marcin/project/service/CustomerService.java
@@ -1,5 +1,6 @@
 package pl.marcin.project.service;
 
+import lombok.extern.slf4j.Slf4j;
 import pl.marcin.project.model.Cup;
 import pl.marcin.project.model.Customer;
 import pl.marcin.project.model.Purchase;
@@ -9,18 +10,18 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 public class CustomerService {
-
-
     private CustomerRepository customerRepository;
 
     public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
     }
 
-    public String addCustomer(Customer customer) {
+    public int addCustomer(Customer customer) {
         customerRepository.saveCustomer(customer);
-        return "Customer " + customer.getId() + " saved";
+        log.info("Customer " + customer.getId() + " saved");
+        return customer.getId();
     }
 
     public String updateCustomer(int customerId, Customer customer) {
@@ -40,6 +41,7 @@ public class CustomerService {
     public Customer getCustomer(int customerId) {
         return customerRepository.findCustomer(customerId);
     }
+
     public String updatePurchaseHistory(Purchase purchase, Customer customer){
         customerRepository.updatePurchaseHistory(purchase,customer);
         return "History updated";

3329dfe52dcfe469857a2600b31e86f7cd26df63
Marcin <gasiorek.mail@gmail.com>
2023-06-20 16:41:48 +0200

    used context in WebAppRunner

--- src/main/java/pl/marcin/project/runners/WebAppRunner.java
+++ src/main/java/pl/marcin/project/runners/WebAppRunner.java
@@ -1,12 +1,16 @@
 package pl.marcin.project.runners;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import pl.marcin.project.ProjectApplication;
+import pl.marcin.project.entityService.CustomerEntityService;
 
 public class WebAppRunner implements AppRunner {
 
     @Override
     public void runApplication() {
-        SpringApplication.run(ProjectApplication.class);
+        ConfigurableApplicationContext context = SpringApplication.run(ProjectApplication.class);
+        var service = context.getBean(CustomerEntityService.class);
+
     }
 }

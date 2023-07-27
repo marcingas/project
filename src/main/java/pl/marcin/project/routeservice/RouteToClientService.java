5994a3daffd21ec2796d0d4f9af8ecf9adfca32f
Marcin <gasiorek.mail@gmail.com>
2023-07-27 13:55:37 +0200

    moved method with adjecency data to test class

--- src/main/java/pl/marcin/project/routeservice/RouteToClientService.java
+++ src/main/java/pl/marcin/project/routeservice/RouteToClientService.java
@@ -5,32 +5,7 @@ import java.util.*;
 public class RouteToClientService {
 
 
-    public List<List<Client>> loadListOfAdjecencyClients() {
 
-        List<Client> shopAdj = new ArrayList<>(List.of(new Client(1, 2), new Client(2, 3)));
-        List<Client> client1 = new ArrayList<>(List.of(new Client(0, 2), new Client(2, 4),
-                new Client(4, 3), new Client(5, 4)));
-        List<Client> client2 = new ArrayList<>(List.of(new Client(0, 3), new Client(1, 4),
-                new Client(3, 2), new Client(4, 2)));
-        List<Client> client3 = new ArrayList<>(List.of(new Client(2, 2), new Client(6, 1)));
-        List<Client> client4 = new ArrayList<>(List.of(new Client(2, 2), new Client(6, 1),
-                new Client(5, 1), new Client(1, 3)));
-        List<Client> client5 = new ArrayList<>(List.of(new Client(1, 4), new Client(6, 2),
-                new Client(4, 1)));
-        List<Client> client6 = new ArrayList<>(List.of(new Client(3, 1), new Client(4, 1),
-                new Client(5, 2), new Client(2, 3)));
-
-        List<List<Client>> adjecencyList = new ArrayList<>();
-        adjecencyList.add(shopAdj);
-        adjecencyList.add(client1);
-        adjecencyList.add(client2);
-        adjecencyList.add(client3);
-        adjecencyList.add(client4);
-        adjecencyList.add(client5);
-        adjecencyList.add(client6);
-
-        return adjecencyList;
-    }
 
     public int[] findShortestRoadToEachClient(List<List<Client>> adjecencyList, int shopAdj) {
         int[] distanceAns = new int[adjecencyList.size()];
--- src/test/java/pl/marcin/project/routeservice/RouteToClientServiceTest.java
+++ src/test/java/pl/marcin/project/routeservice/RouteToClientServiceTest.java
@@ -11,11 +11,38 @@ import static org.junit.jupiter.api.Assertions.*;
 class RouteToClientServiceTest {
     RouteToClientService routeToClientService = new RouteToClientService();
 
+    public List<List<Client>> loadListOfAdjecencyClients() {
+
+        List<Client> shopAdj = new ArrayList<>(List.of(new Client(1, 2), new Client(2, 3)));
+        List<Client> client1 = new ArrayList<>(List.of(new Client(0, 2), new Client(2, 4),
+                new Client(4, 3), new Client(5, 4)));
+        List<Client> client2 = new ArrayList<>(List.of(new Client(0, 3), new Client(1, 4),
+                new Client(3, 2), new Client(4, 2)));
+        List<Client> client3 = new ArrayList<>(List.of(new Client(2, 2), new Client(6, 1)));
+        List<Client> client4 = new ArrayList<>(List.of(new Client(2, 2), new Client(6, 1),
+                new Client(5, 1), new Client(1, 3)));
+        List<Client> client5 = new ArrayList<>(List.of(new Client(1, 4), new Client(6, 2),
+                new Client(4, 1)));
+        List<Client> client6 = new ArrayList<>(List.of(new Client(3, 1), new Client(4, 1),
+                new Client(5, 2), new Client(2, 3)));
+
+        List<List<Client>> adjecencyList = new ArrayList<>();
+        adjecencyList.add(shopAdj);
+        adjecencyList.add(client1);
+        adjecencyList.add(client2);
+        adjecencyList.add(client3);
+        adjecencyList.add(client4);
+        adjecencyList.add(client5);
+        adjecencyList.add(client6);
+
+        return adjecencyList;
+    }
+
     @Test
     void findShortestRoadToEachClient() {
         //given
         int[] expectedRoute = new int[]{0, 2, 3, 5, 5, 6, 6};
-        List<List<Client>> adjecencyList = routeToClientService.loadListOfAdjecencyClients();
+        List<List<Client>> adjecencyList = loadListOfAdjecencyClients();
         int shop = 0;
         //when
         int[] route = routeToClientService.findShortestRoadToEachClient(adjecencyList, shop);
@@ -30,7 +57,7 @@ class RouteToClientServiceTest {
     void printFullPath() {
         //given
         int[] expectedRoute = new int[]{0, 2, 3, 5, 5, 6, 6};
-        List<List<Client>> adjecencyList = routeToClientService.loadListOfAdjecencyClients();
+        List<List<Client>> adjecencyList = loadListOfAdjecencyClients();
         int shop = 0;
         int targetClient = 3;
         List<Integer> expectedAns = new ArrayList<>(List.of(0, 2, 3));
